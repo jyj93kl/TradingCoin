53e2861c2a8946483e056154c85d8d4778571922
yjjeong <65210145+sinsiway@users.noreply.github.com>
2021-04-13 10:33:17 +0900

    admin html add

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,7 @@ module.exports = (function (app){
   app.get('/', function(req, res) {
     res.render('index.html');
   });
+  app.get('/admin', function(req, res) {
+    res.render('admin.html');
+  });
 });
