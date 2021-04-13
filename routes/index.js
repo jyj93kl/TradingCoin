a1e043bd45f8f701882b85b2516a8da572a1801d
jyjang <jyjang@sinsiway.com>
2021-04-13 12:37:00 +0900

    일부 수정

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ module.exports = (function (app){
   app.get('/', function(req, res) {
     res.render('index.html');
   });
-  app.get('/admin', function(req, res) {
-    res.render('admin.html');
+  app.get('/moblie', function(req, res) {
+    res.render('moblie_main.html');
   });
 });
